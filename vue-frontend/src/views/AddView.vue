2ddf352e121cd9be0db046217329b4bdf3087414
BrandonTang89 <tangyuhanbrandon@gmail.com>
2022-11-16 05:31:46 +0800

    created Vue Front-End

---
+++ vue-frontend/src/views/AddView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="about">
+      <h1>Add a Problem</h1>
+      <h2>This part will eventually become a(nother) form?</h2>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  
